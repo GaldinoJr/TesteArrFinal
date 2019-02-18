a6f85f47ec97223ad49f4663f2f533a9edaf35d0
galdinojr <galdinodbz@gmail.com>
2019-02-18 15:52:10 -0300

    Initial commit

---
+++ app/src/main/java/com/galdinorodrigues/testearrfinal/MainActivity.java
@@ -0,0 +1,13 @@
+package com.galdinorodrigues.testearrfinal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
